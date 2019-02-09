0b75d50591490926ed48dd066ebed501d28d340c
eliut98 <roberthgon18@gmail.com>
2019-02-09 16:42:54 +0100

    createElement

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 // Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/*
+const title = React.createElement('h1', {}, 'My First React Code');
+const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
+const container = React.createElement('div', {}, [title, paragraph]);
+*/
+
+const list = React.createElement('div', {}, 
+    React.createElement('h1', {}, 'My favorite ice cream flavors'),
+    React.createElement('ul', {}, 
+        [
+            React.createElement('li', { className: 'brown' }, 'Chacolate'),
+            React.createElement('li', {}, 'Vanilla'),
+            React.createElement('li', {}, 'Banana'),
+        ]
+    )
+);
+
+ReactDOM.render(
+    list,
+    document.getElementById('global')
+);
